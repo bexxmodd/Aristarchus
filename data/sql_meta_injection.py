7198a25ddc13520cbba294ec1d4a76d3829373c9
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2020-06-09 15:37:06 -0400

    Finished the script to inject metadata into books.db

--- data/sql_meta_injection.py
+++ data/sql_meta_injection.py
@@ -1,35 +1,29 @@
 import sqlite3
+import numpy as np
 from xmlCleanDelete import extract_meta
 
 
 s = extract_meta()
 
-                # metadata['book_id']
-                # metadata['title'].
-                # metadata['author'].
-                # metadata['isbn13'].
-                # metadata['published']
-                # metadata['pages'].
-                # metadata['avg_rating'].
-                # metadata['count'].
-                # metadata['lang']
-                # metadata['cover_url']
-# data = [(int(b), t, a, int(i), d, int(p), float(r), int(c), l) for b, t, a, i, d, p, r, c, l in zip(s['book_id'], s['title'], s['author'], s['isbn13'],
-#                                                         s['published'], s['pages'], s['avg_rating'], s['count'], s['lang'])]
-dat = []
-for b, t, a, i, d, p, r, c, l in zip(s['book_id'], s['title'], s['author'], s['isbn13'], s['published'],
-                                        s['pages'], s['avg_rating'], s['count'], s['lang']):
-    if None in (b, i, p, r, c):
-        dat.append(None)
-    else:
-        dat.append((int(b), t, a, int(i), d, int(p), float(r), int(c), l))
+# Extrapolate the data as the set of values into the list.
+data = []
+for b, t, a, i, y, p, r, c, lan, u in zip(s['book_id'], s['title'], s['author'], s['isbn13'], s['published'], s['pages'], s['avg_rating'], s['count'], s['lang'], s['cover_url']):
+    data.append((int(b), t, a, i, y, int(p or 0), float(r or 0), int(c or 0), lan, u))
 
-print(dat)
-# # Innitiate connection with a database
-# conn = sqlite3.connect('/home/bexx/Projects/Aristarchus/data/books.db')
+# Innitiate connection with a database.
+conn = sqlite3.connect('/home/bexx/Projects/Aristarchus/data/books.db')
 
-# # Create a cursor
-# c = conn.cursor()
+# Create a cursor.
+c = conn.cursor()
 
-# meta_table_create = "CREATE TABLE IF NOT EXISTS metadata (book_id INT PRIMARY KEY, title TEXT, author TEXT, isbn13 TEXT, published TEXT, pages TEXT, avg_rating DOUBLE, count INT, lang TEXT)"
-# c.execute(meta_table_create)
+# Create metadata table.
+create_mt = "CREATE TABLE IF NOT EXISTS metadata (book_id INTEGER PRIMARY KEY, title TEXT, author TEXT, isbn13 TEXT, published TEXT, pages INTEGER, avg_rating REAL, count INT, lang TEXT, cover_url TEXT)"
+c.execute(create_mt)
+
+# Inject data into the metadata table.
+q = "INSERT INTO metadata (book_id, title, author, isbn13, published, pages, avg_rating, count, lang, cover_url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+c.executemany(q, data)
+
+# Commit and close the connection.
+conn.commit()
+conn.close()
